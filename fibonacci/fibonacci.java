23b4f3c428f19d7054c54ddbe5da131075e87bf2
MauryAzura <azuralynux@gmail.com>
2023-12-27 16:03:24 -0600

    5 commit

--- fibonacci/fibonacci.java
+++ fibonacci/fibonacci.java
@@ -4,7 +4,19 @@ public class fibonacci {
     
     public static void main(String[] args) {
         
-        
+    long num = 0;
+    long num2 = 1;
+    long a = 0;
+
+    for(int i=0;i<50;i++){
+
+        System.out.println(num);
+
+        a = num + num2;
+        num = num2;
+        num2 = a;
+
+    }
 
     }
 
