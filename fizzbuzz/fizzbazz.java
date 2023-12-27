8a119eab663835361c94a2b3bb8425fbae7f0f6d
MauryAzura <azuralynux@gmail.com>
2023-12-26 20:31:33 -0600

    1 commit

---
+++ fizzbuzz/fizzbazz.java
@@ -0,0 +1,30 @@
+package fizzbuzz;
+public class fizzbazz{
+    public static void main(String[] mauricio) {
+
+    for(byte i=1;i<=100;i++){
+
+        if((i%3==0)&&(i%5==0)){
+
+            System.out.println("fizzbuzz");
+
+        }else if(i%3==0){
+
+            System.out.println("fizz");
+
+        }else if(i%5==0){
+
+            System.out.println("buzz");
+
+        }else{
+
+            System.out.println(i);
+
+        }
+
+    }
+
+    }
+
+
+}
