c883bcb86d406cb3f029cb544b367c1897b60d0f
Maury SO <128880883+MauryAzura@users.noreply.github.com>
2023-12-28 23:44:42 -0600

    Actualizar fizzbazz.java

--- fizzbuzz/fizzbazz.java
+++ fizzbuzz/fizzbazz.java
@@ -2,6 +2,8 @@ package fizzbuzz;
 public class fizzbazz{
     public static void main(String[] mauricio) {
 
+    System.out.println("fizzbuzz");
+
     for(byte i=1;i<=100;i++){
 
         if((i%3==0)&&(i%5==0)){
