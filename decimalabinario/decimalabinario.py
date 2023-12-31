685f5a10c67fdf4f1ef48a890bc1b50b2568fbd1
MauryAzura <azuralynux@gmail.com>
2023-12-31 13:52:47 -0600

    16 commit

--- decimalabinario/decimalabinario.py
+++ decimalabinario/decimalabinario.py
@@ -1,6 +1,15 @@
-
-n = int(input("Escribe un numero para convertirlo a binsrio"));
-
-while n>=0:
+"""
+ * Crea un programa se encargue de transformar un número
+ * decimal a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
+ """
+n = int(input("Escribe un numero para convertirlo a binsrio: "));
+b=""
+while n>0:
     if n%2==0:
+        b = "0" + b
+    else:
+        b = "1" + b
         
+    n//=2
+    
+print(b)
