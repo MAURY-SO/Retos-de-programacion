8af8077cdd523182bf8e062dfeb8496fe4212a22
MauryAzura <azuralynux@gmail.com>
2023-12-30 21:22:57 -0600

    10 commit

--- numeroprimo/primo.java
+++ numeroprimo/primo.java
@@ -1,3 +1,7 @@
+/*
+ * Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */
 package numeroprimo;
 import javax.swing.JOptionPane;
 
@@ -6,17 +10,35 @@ public class primo {
     public static void main(String[] args) {
         
     int n = Integer.parseInt(JOptionPane.showInputDialog(null,"Escribe un numero:")); 
+    boolean esPrimo = esNumeroPrimo(n);
+        
+    if (esPrimo) {
+        System.out.println(n + " es un número primo.");
+    } else {
+        System.out.println(n + " no es un número primo.");
+    }
 
-    if((n%2==0)||(n%3==0)||(n%5==0)||(n%7==0)||(n%11==0)){
-
-        JOptionPane.showMessageDialog(null,"El numero "+n+" no es primo");
-
-    }else{
-
-        JOptionPane.showMessageDialog(null,"El numero "+n+" es primo");
+    for (int i = 2; i < 100; i++) {
+        if (esNumeroPrimo(i)) {
+            System.out.print(i + " ");
+        }
+    }
 
     }
 
+    public static boolean esNumeroPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+        
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 
 }
+
