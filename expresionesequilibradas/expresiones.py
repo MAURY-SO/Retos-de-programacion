25cf1236167ab15aadf69dbeeeca5907a3907217
MauryAzura <azuralynux@gmail.com>
2024-01-01 23:50:21 -0600

    24 commit

--- expresionesequilibradas/expresiones.py
+++ expresionesequilibradas/expresiones.py
@@ -2,7 +2,8 @@ exp = input("Ingresa una expresión: ")
 
 p_d = ["(", ")", "{", "}", "[", "]"]
 dic_aux = []
-
+co_dic = []
+c = 0
 
 p_d.sort()
 set_d = set(p_d)
@@ -10,12 +11,15 @@ set_e = set(exp)
 
 exp_aux = sorted(set_d.intersection(set_e))
 
-print(len(exp_aux))
+if(len(exp_aux)%2!=0):
+    c=len(exp_aux)+1
+else:
+    c=len(exp_aux)
+    
+for i in range(0,c):
+    co_dic.append(p_d[i])
 
-
-print(p_d)
-
-if exp_aux == p_d:
+if exp_aux == co_dic:
     print("Expresion balanceada")
 else:
     print("Expresion no balanceada")
