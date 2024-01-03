22a6cc7bb2e6707774de66ad791b013d39593e0d
MauryAzura <azuralynux@gmail.com>
2024-01-02 20:18:23 -0600

    28 commit

---
+++ palindromo/palindromo.java
@@ -0,0 +1,17 @@
+package palindromo;
+
+import javax.swing.JOptionPane;
+
+public class palindromo {
+    public static void main(String[] args) {
+        
+        String p = JOptionPane.showInputDialog(null,"Ingresa una frase:");
+
+        p = p.toLowerCase();
+
+        char [] parray = p.toCharArray(); 
+
+    }
+
+    
+}
--- palindromo/palindromo.py
+++ palindromo/palindromo.py
@@ -1,15 +1,12 @@
 
 p = input("ingrese una frase: ")
 
-p.lower()
+p = p.lower()
 
 array_aux = ''.join(p.split())
 array = list(array_aux)
 
 array_aux = array[::-1]
-
-print(array)
-print(array_aux)
 
 if array_aux==array:
     print("La frase es un palindromo")
