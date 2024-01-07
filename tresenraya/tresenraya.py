f2d38e73802221099a0cc70a7915044dfcb86af9
MauryAzura <azuralynux@gmail.com>
2024-01-06 23:02:55 -0600

    34 commit

--- tresenraya/tresenraya.py
+++ tresenraya/tresenraya.py
@@ -21,8 +21,8 @@ def ingresar(cuadro, v1,v2):
     va1 = v1
     va2 = v2
     while True:
-        x=int(input("Que posicion en x '"+va1+"' ?"))
-        y=int(input("Que posicion en y '"+va1+"' ?"))
+        x=int(input("Que posicion en x '"+va1+"' ? "))
+        y=int(input("Que posicion en y '"+va1+"' ? "))
         cuadro[y][x] = va1
         print("  0 1 2 <-x")
         for x in range (0,3):
@@ -30,10 +30,14 @@ def ingresar(cuadro, v1,v2):
             for y in range(0,3):
                 print(cuadro[x][y],end="|")
             print()
-           
-        if cuadro[0][0] == cuadro[1][1] == cuadro[2][2] == va1:
-            print("Gano "+va1)
-            break
+        
+        for i in range(3):
+            if cuadro[i][0] == cuadro[i][1] == cuadro[i][2] == va1 or \
+               cuadro[0][i] == cuadro[1][i] == cuadro[2][i] == va1 or \
+               cuadro[0][0] == cuadro[1][1] == cuadro[2][2] == va1 or \
+               cuadro[0][2] == cuadro[1][1] == cuadro[2][0] == va1:
+                print("Gano la '"+va1+"' !!!")
+                return
             
         aux = va1
         va1 = va2
