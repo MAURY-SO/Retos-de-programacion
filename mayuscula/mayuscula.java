8c6ca469678903d24c1a00f6ab751a321b90ef01
MauryAzura <azuralynux@gmail.com>
2024-01-06 19:41:29 -0600

    38 commit

--- mayuscula/mayuscula.java
+++ mayuscula/mayuscula.java
@@ -4,21 +4,40 @@ public class mayuscula {
     public static void main(String[] args) {
         
         String pal = JOptionPane.showInputDialog(null,"Ingresa una frase en minuscula");
+        String [] frase = pal.split(" ");
 
-        String [] p = pal.split(" ");
+        for(int i=0;i<frase.length;i++){
 
-        System.out.println(Mayus(p,0));
+             System.out.print(Mayus(frase,i)+" ");
 
-    }
+        }
 
-    public static String Mayus(String palabra){
-
-        char aux[] = palabra.toCharArray();
+       
         
-        System.out.println(aux);
-
-        return palabra;
     }
 
+    public static String Mayus(String[] args,Integer index){
+
+        String pal = args[index];
+        char [] palabra = pal.toCharArray();
+        char [] dic = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        char [] dic2 = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        
+
+        for(int i=0; i<dic.length;i++){
+
+            if(palabra[0]==dic[i]){
+
+                palabra[0]=dic2[i];
+
+            }
+
+        }
+
+        String salida = new String(palabra);
+
+        return salida;
+
+    }
     
 }
