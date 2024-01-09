3c84cae249d1ad480df0a5e1360a27362f2b5d2d
MauryAzura <azuralynux@gmail.com>
2024-01-08 19:10:32 -0600

    36 commit

--- conversordetiempo/conversor.py
+++ conversordetiempo/conversor.py
@@ -1,2 +1,11 @@
 def Conversor(dias, horas, minutos, segundos):
     
+    v1 = dias * 24 * 60 * 60 * 1000
+    v2 = horas * 60 * 60 * 1000
+    v3 = minutos * 60 * 1000
+    v4 = segundos * 1000
+    
+    return v1 + v2 + v3 + v4
+
+
+print(str(Conversor(24,0,0,1))+" milisegundos")
